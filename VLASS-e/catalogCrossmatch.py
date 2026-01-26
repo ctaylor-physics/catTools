c932fa9ad09102f1113ecc6d46cfcce0494ec3f3
ctaylor-physics <ctaylor98@unm.edu>
2026-01-26 16:55:12 -0700

    fixed ADQL inputs

--- VLASS-e/catalogCrossmatch.py
+++ VLASS-e/catalogCrossmatch.py
@@ -4,9 +4,23 @@
 ###
 
 import pandas as pd
+import numpy as np
+import pyvo as vo
+
 
 def main():
-    print('catalogs go here')
+    print('Testing TAP')
+    tap_service = vo.dal.TAPService("https://exoplanetarchive.ipac.caltech.edu/TAP")
+    query = """
+        SELECT TOP 5
+            sy_name, hostname, ra, dec, gaia_dr2_id
+        FROM
+            stellarhosts
+        WHERE 
+            sy_pnum = 2
+        """
+    results = tap_service.search(query)
+    print(results)
     return
 
 if __name__ == "__main__":
